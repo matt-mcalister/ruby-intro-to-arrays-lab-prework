7f35e9998d7dce4eafc75a380fd6459722392295
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-27 01:11:29 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,15 +7,15 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array(0)
+  array[0]
 end
 
 def third_element(array)
-  array(2)
+  array[2]
 end
 
 def last_element(array)
-  array(-1)
+  array[-1]
 end
 
 
